bacdf34075568a8e16ca028081573e855a374e5b
Callisto95 <luluklzde@gmail.com>
2025-04-08 17:23:15 +0200

    whitespace

--- main.cpp
+++ main.cpp
@@ -26,22 +26,22 @@ bool isGreaterThan(Type left, Type right) {
 
 int main() {
 	using namespace std;
-
+	
 	vector<double> vec = {1.1,3,2,4,8,6,7};
 	vec.push_back(5);
-
+	
 	auto lessSort = [&vec](){sort(vec.begin(), vec.begin() + vec.size()/2, isLessThan<double>);};
 	auto greaterSort = [&vec](){sort(vec.begin() + vec.size()/2, vec.end(), isGreaterThan<double>);};
-
+	
 	thread lessSorter(lessSort);
 	thread greaterSorter(greaterSort);
-
+	
 	lessSorter.join();
 	greaterSorter.join();
-
+	
 	for (double item : vec) {
 		cout << item << " ";
 	}
-
+	
 	return 0;
 }
