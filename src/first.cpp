c96a4cf9c36f3fe6a6d6003fd70eeaa43fe5190f
Callisto95 <luluklzde@gmail.com>
2025-04-10 23:54:22 +0200

    make CLion happy

--- src/first.cpp
+++ src/first.cpp
@@ -5,45 +5,41 @@
 #include <vector>
 
 class Person {
-  friend bool operator<(Person const &p, Person const &other) {
-    return p.surname < other.surname;
-  }
+    friend bool operator<(Person const& p, Person const& other) { return p.surname < other.surname; }
 
 public:
-  std::string firstname;
-  std::string surname;
+    std::string firstname;
+    std::string surname;
 };
 
-template <class Type> bool isLessThan(Type left, Type right) {
-  return left < right;
+template <class Type>
+bool isLessThan(Type left, Type right) {
+    return left < right;
 }
 
-template <class Type> bool isGreaterThan(Type left, Type right) {
-  return left > right;
+template <class Type>
+bool isGreaterThan(Type left, Type right) {
+    return left > right;
 }
 
 int main() {
-  using namespace std;
+    using namespace std;
 
-  vector<double> vec = {1.1, 3, 2, 4, 8, 6, 7};
-  vec.push_back(5);
+    vector<double> vec = {1.1, 3, 2, 4, 8, 6, 7};
+    vec.push_back(5);
 
-  auto lessSort = [&vec]() {
-    sort(vec.begin(), vec.begin() + vec.size() / 2, isLessThan<double>);
-  };
-  auto greaterSort = [&vec]() {
-    sort(vec.begin() + vec.size() / 2, vec.end(), isGreaterThan<double>);
-  };
+    auto lessSort = [&vec]() { sort(vec.begin(), vec.begin() + vec.size() / 2, isLessThan<double>); };
+    auto greaterSort = [&vec]() { sort(vec.begin() + vec.size() / 2, vec.end(), isGreaterThan<double>); };
 
-  thread lessSorter(lessSort);
-  thread greaterSorter(greaterSort);
+    thread lessSorter(lessSort);
+    thread greaterSorter(greaterSort);
 
-  lessSorter.join();
-  greaterSorter.join();
+    lessSorter.join();
+    greaterSorter.join();
 
-  for (double item : vec) {
-    cout << item << " ";
-  }
+    for (double item : vec) {
+        cout << item << " ";
+    }
 
-  return 0;
+    return 0;
 }
