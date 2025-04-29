fce9de2a043ac5c8364713a1864325eb9f7fa4ba
Luca Saalfeld <l.saalfeld@tu-braunschweig.de>
2025-04-29 15:53:22 +0200

    B1-A6: fix and result

--- blatt1/main.cpp
+++ blatt1/main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int main() {
 	cout << "Hello, World!" << endl;
 
-	cout << "Wir sind: " << team[0] << " ... " << team.at(teamSize) << endl;
 	vector<string> team = {"Paddy", "Linda"}; // Vektor mit Startwerten
 	team.emplace_back("Steve");				  // weiteren Wert zum Vektor hinzufügen
 	team.emplace_back("Flo");				  // ...
 	int teamSize = team.size();
 
+	cout << "Wir sind: " << team[0] << " ... " << team.at(teamSize - 1) << endl;
 	return 0;
 }
