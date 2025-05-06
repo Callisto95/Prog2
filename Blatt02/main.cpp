f3f1bb8a41be044bd268bada81cffce16f97d66b
Mathis Benecke <mathisbenecke@Mathis-Mac.local>
2025-05-06 15:29:57 +0200

    B2-A1: solution

--- Blatt02/main.cpp
+++ Blatt02/main.cpp
@@ -10,18 +10,24 @@ using std::string;
 
 // TODO Aufgabe 1:
 //  Definiert die Funktion `inputNextSerialNumber()` so, dass sie eine Seriennummer als `string` auf der Konsole einliest und diese zurückgibt.
-/*
- ???
- */
+
+string inputNextSerialNumber(){
+
+    std::cout << "Bitte Seriennummer eingeben: " << std::flush;
+
+    std::string serialNumber;
+    std::getline(std::cin, serialNumber);
+
+    return serialNumber;
+
+}
 
 // TODO Aufgabe 3:
 //  Implementiert die Funktion `checkSerialNumberAndPrintResult(serialNumber)`.
 //  Sie soll mithilfe der Funktion `getEuroSerialNumberVersion(serialNumber)` ermitteln, um welche Version einer Euro-Banknote es sich handelt, und die entsprechende Jahreszahl in der Konsole ausgeben.
 //  Anschliessend soll in einer Fallunterscheidung (switch) anhand der Jahreszahl die entsprechende Funktion `checkEuroSerialNumber2013/2002(serialNumber)` aufgerufen und das Ergebnis (Gueltig/Ungueltig) auf der Konsole ausgeben werden.
 void checkSerialNumberAndPrintResult(string & serialNumber) {
-    /*
-     ???
-     */
+
 }
 
 int main() {
@@ -33,9 +39,8 @@ int main() {
 
         // TODO Aufgabe 1:
         //  Ruft `inputNextSerialNumber()` auf, um eine Seriennummer einzugeben.
-        /*
-         ???
-         */
+        string serialNumber;
+        inputNextSerialNumber();
 
         // TODO Aufgabe 4:
         //  Das Programm soll beendet werden, wenn statt einer Seriennummer "fertig" eingegeben wurde.
