46fd59ae6d4d2d61f87771dba58f65f54dc0e6a0
Luca Saalfeld <l.saalfeld@tu-braunschweig.de>
2025-05-13 23:36:58 +0200

    B3-A2: solution

--- blatt3/src/libSea/Coordinates.cpp
+++ blatt3/src/libSea/Coordinates.cpp
@@ -3,29 +3,22 @@
 
 namespace Sea {
 
-    Coordinates::Coordinates(unsigned int newX, unsigned int newY)
-    : x(newX), y(newY)
-    {
-    }
+	Coordinates::Coordinates(const unsigned int newX, const unsigned int newY) : x(newX), y(newY) {}
 
-    bool Coordinates::samePositionAs(unsigned int otherX, unsigned int otherY) const
-    {
-        return x == otherX && y == otherY;
-    }
+	bool Coordinates::samePositionAs(const unsigned int otherX, const unsigned int otherY) const {
+		return x == otherX && y == otherY;
+	}
 
-    bool Coordinates::samePositionAs(Coordinates const & otherCoordinates) const
-    {
-        return samePositionAs(otherCoordinates.x, otherCoordinates.y);
-    }
+	bool Coordinates::samePositionAs(Coordinates const& otherCoordinates) const {
+		return samePositionAs(otherCoordinates.x, otherCoordinates.y);
+	}
 
-    unsigned int Coordinates::getX() const
-    {
-        return x;
-    }
+	unsigned int Coordinates::getX() const {
+		return x;
+	}
 
-    unsigned int Coordinates::getY() const
-    {
-        return y;
-    }
+	unsigned int Coordinates::getY() const {
+		return y;
+	}
 
-}
+} // namespace Sea
--- blatt3/src/libSea/Object.cpp
+++ blatt3/src/libSea/Object.cpp
@@ -1,31 +1,53 @@
 #include "Object.h"
 
+#include <algorithm>
+#include <vector>
+
+using Sea::Coordinates;
+using Sea::Object;
+using Sea::Orientation;
+using std::vector;
+
+vector<Coordinates> getAllCoordinates(Coordinates const& anchor, const unsigned int size,
+									  Orientation const orientation) {
+
+	std::vector<Coordinates> coordinates;
+
+	for (unsigned int offset = 0; offset < size; offset++) {
+		if (orientation == Orientation::X) {
+			coordinates.emplace_back(anchor.getX() + offset, anchor.getY());
+		} else {
+			coordinates.emplace_back(anchor.getX(), anchor.getY() + offset);
+		}
+	}
+
+	return coordinates;
+}
 
 namespace Sea {
 
-    Object::Object(Coordinates const & newCoordinates, unsigned int newSize, Sea::Orientation newOrientation)
-    : Coordinates(newCoordinates), size(newSize), orientation(newOrientation)
-    {
-    }
+	Object::Object(Coordinates const& newCoordinates, const unsigned int newSize, const Orientation newOrientation) :
+		Coordinates(newCoordinates),
+		size(newSize),
+		orientation(newOrientation) {}
 
-    bool Object::atCoordinates(Coordinates const & coordinates) const
-    {
-        // TODO Aufgabe 2:
-        //  Nutzt die Funktionalität von `Coordinates`, um zu bestimmen, ob ein Teil dieses Objekts auf den übergebenen Koordinaten (`coordinates`) liegt.
-        return false;
-        /*
-         ???
-         */
-    }
+	bool Object::atCoordinates(Coordinates const& coordinates) const {
+		// TODO Aufgabe 2:
+		//  Nutzt die Funktionalität von `Coordinates`, um zu bestimmen, ob ein Teil dieses Objekts auf den übergebenen
+		//  Koordinaten (`coordinates`) liegt.
 
-    bool Object::intersectsWith(Object const & otherObject) const
-    {
-        // TODO Aufgabe 2:
-        //  Nutzt die Funktion `atCoordinates(..)`, um zu bestimmen, ob sich dieses und das übergebene Objekt (`otherObject`) überschneiden.
-        return false;
-        /*
-         ???
-         */
-    }
+		vector<Coordinates> points = getAllCoordinates(*this, size, orientation);
+		return std::any_of(points.begin(), points.end(),
+						   [&coordinates](Coordinates const& point) { return coordinates.samePositionAs(point); });
+	}
 
-}
+	bool Object::intersectsWith(Object const& otherObject) const {
+		// TODO Aufgabe 2:
+		//  Nutzt die Funktion `atCoordinates(..)`, um zu bestimmen, ob sich dieses und das übergebene Objekt
+		//  (`otherObject`) überschneiden.
+
+		vector<Coordinates> points = getAllCoordinates(otherObject, otherObject.size, otherObject.orientation);
+		return std::any_of(points.begin(), points.end(),atCoordinates);
+	}
+
+} // namespace Sea
