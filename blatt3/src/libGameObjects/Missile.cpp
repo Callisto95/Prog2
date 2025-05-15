70d80770de46d307f617f4a44ec07c55d38a07fe
Marcel Bryzek <m.bryzek@tu-braunschweig.de>
2025-05-15 20:14:55 +0200

    B3-A3: solution

--- blatt3/src/libGameObjects/Missile.cpp
+++ blatt3/src/libGameObjects/Missile.cpp
@@ -2,6 +2,16 @@
 
 // TODO Aufgabe 3:
 //  Definiert die Funktionen der Klasse `Missile`.
-/*
- ???
- */
+namespace GameObjects {
+	Missile::Missile(Coordinates const& coordinates) : Coordinates(coordinates) {}
+
+	void Missile::hitSomething() {
+		isHit = true;
+	}
+
+	bool Missile::hasHitSomething() const {
+		return isHit;
+	}
+
+
+} // namespace GameObjects
--- blatt3/src/libGameObjects/Missile.h
+++ blatt3/src/libGameObjects/Missile.h
@@ -6,34 +6,33 @@
 
 
 // TODO Aufgabe 3:
-//  Implementiert die Klasse `Missile` als Erweiterung (Vererbung) der Klasse `Coordinates`, im Namensraum `GameObjects`.
+//  Implementiert die Klasse `Missile` als Erweiterung (Vererbung) der Klasse `Coordinates`, im Namensraum
+//  `GameObjects`.
 namespace GameObjects {
 
 	class Missile : public Sea::Coordinates {
 
 		bool isHit = false;
-        // TODO Aufgabe 3:
-        //  Deklariert einen Konstruktor, sodass Missiles mit `Coordinates` erstellt werden müssen.
+		// TODO Aufgabe 3:
+		//  Deklariert einen Konstruktor, sodass Missiles mit `Coordinates` erstellt werden müssen.
+	public:
+		Missile(Coordinates const& coordinates);
 
-		Missile(Coordinates coordinates);
+		// TODO Aufgabe 3:
+		//  Deklariert eine Funktion `void hitSomething()`, die diese Instanz als Treffer markiert.
+		//  (Dazu ist eine zusätzliche Membervariable nötig)
+		void hitSomething();
 
-        // TODO Aufgabe 3:
-        //  Deklariert eine Funktion `void hitSomething()`, die diese Instanz als Treffer markiert.
-        //  (Dazu ist eine zusätzliche Membervariable nötig)
-        void hitSomething();
-
-        // TODO Aufgabe 3:
-        //  Deklariert eine Funktion `bool hasHitSomething() const`, die prüft, ob diese Instanz ein Treffer war.
-        /*
-         ???
-         */
+		// TODO Aufgabe 3:
+		//  Deklariert eine Funktion `bool hasHitSomething() const`, die prüft, ob diese Instanz ein Treffer war.
+		bool hasHitSomething() const;
 	};
-}
+} // namespace GameObjects
 
 
 // TODO Aufgabe 3:
 //  Entfernt die Kommentar-Zeichen (//) vor `#define CLASS_MISSILE_COMPLETE` wenn Aufgabe 3 erledigt ist:
-// #define CLASS_MISSILE_COMPLETE
+#define CLASS_MISSILE_COMPLETE
 
 
-#endif //BLATT3_MISSILE_H
+#endif // BLATT3_MISSILE_H
